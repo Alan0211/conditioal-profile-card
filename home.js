215463fd9a1f52150a0502d705754eff52231bed
Alan0211 <84466053+Alan0211@users.noreply.github.com>
2021-09-03 11:40:27 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NombreYapellido from "./NombreYapellido";
+export const Home = () => {
+	return (
+		<>
+			<div className="container">
+				<ul>
+					<NombreYapellido />
+				</ul>
+			</div>
+		</>
+	);
+};
